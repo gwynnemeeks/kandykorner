12e8a50aaa131b9e88dd83fbef8b62f7843585ea
Gwynne Meeks <gwynnemeeks@gmail.com>
2021-06-10 21:32:31 -0500

    wip app main page

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./KandyKorner.css"
+
+export const KandyKorner = () => (
+    <>
+        <h2>Kandy Korner</h2>
+        <small>Satisfying your sweet tooth one sweet at a time</small>
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>123 Skittles Way</div>
+        </address>
+    </>
+)
