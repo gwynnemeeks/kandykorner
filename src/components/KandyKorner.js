7845ba9fe8c1a12f359b221e934723250d2b62a3
Gwynne Meeks <gwynnemeeks@gmail.com>
2021-06-15 14:40:58 -0500

    wip location

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -4,7 +4,7 @@ import "./KandyKorner.css"
 export const KandyKorner = () => (
     <>
         <h2>Kandy Korner</h2>
-        <small>Satisfying your sweet tooth one sweet at a time</small>
+        <small>Satisfying your sweet tooth one tooth at a time</small>
         <address>
             <div>Visit Us at the Nashville North Location</div>
             <div>123 Skittles Way</div>
---
+++ src/components/locations/Location.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Location.css";
+
+export const Location = () => (
+    <section className="location">
+        <h3 className="location__address">Address:</h3>
+        <div className="location__squareFeet">Square Feet:</div>
+        <div className="location__handicapAccessible">True or False</div>
+    </section>
+)
