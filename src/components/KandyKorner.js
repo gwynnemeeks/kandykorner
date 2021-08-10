731e1c1ee71bcfceea44356c3cce94bed2473f78
Gwynne Meeks <gwynnemeeks@gmail.com>
2021-08-10 15:16:28 -0500

    wip location list

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,20 +1,14 @@
 import React from "react"
-import { Location } from "./locations/Location"
+import { LocationList } from "./locations/LocationList"
 import "./KandyKorner.css"
 
 export const KandyKorner = () => (
     <>
         <h2>Kandy Korner</h2>
         <small>Satisfying your sweet tooth one tooth at a time</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>123 Skittles Way</div>
-        </address>
-
         <h2>Locations</h2>
         <article className="locations">
-            <Location />
+            <LocationList />
         </article>
     </>
 )
